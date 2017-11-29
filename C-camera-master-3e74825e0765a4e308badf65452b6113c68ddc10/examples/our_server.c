01c0f8577adb36072a2dc0fbe67dff3ac46d76e0
unknown <anna_sjovall96@hotmail.com>
2017-11-29 15:04:39 +0100

    removed comments

--- C-camera-master-3e74825e0765a4e308badf65452b6113c68ddc10/examples/our_server.c
+++ C-camera-master-3e74825e0765a4e308badf65452b6113c68ddc10/examples/our_server.c
@@ -9,48 +9,17 @@
 #include <netinet/in.h>
 #include "camera.h"
 
-
-// int try_open_camera(struct global_state* state)
-// {
-//     state->cam = camera_open();
-//     if (!state->cam){ // Check if null
-//         printf("axism3006v: Stream is null, can't connect to camera");
-//         return ERR_OPEN_STREAM;
-//     }
-//     return 0;
-// }
-
 int main(int argc, char *argv[])
 {
-  //Struct to hold IP Address and Port Numbers
   struct sockaddr_in servaddr;
-  //Vill skicka över AF_INET, typen SOCK_STREAM sen 0,
-  //Each server needs to “listen” for connections. The above
-  //function creates a socket with AF_INET ( IP Addressing )
-  //and of type SOCK_STREAM. Data from all devices wishing to
-  //connect on this socket will be redirected to listen_fd.
   int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-  //Clear servaddr ( Mandatory ).
   bzero( &servaddr, sizeof(servaddr));
-  // Set Addressing scheme to – AF_INET ( IP )
-  // Allow any IP to connect – htons(INADDR_ANY)
-  // Listen on port 22000 – htons(22000)
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htons(INADDR_ANY);
   servaddr.sin_port = htons(22000);
-  //Prepare to listen for connections from address/port
-  //specified in sockaddr ( Any IP on port 22000 ).
   bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-  // Start Listening for connections , keep at the most 10 connection
-  // requests waiting.If there are more than 10 computers wanting to connect
-  // at a time, the 11th one fails to.
   while (1) {
     listen(listen_fd, 10);
-    //Accept a connection from any device who is willing to connect, If there
-    //is no one who wants to connect , wait. A file descriptor is returned.
-    //This can finally be used to communicate , whatever is sent by the device
-    // accepted can be read from comm_fd, whatever is written to comm_fd is
-    //sent to the other device.
     int comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
     camera* cam = camera_open();
     frame* camera_frame = camera_get_frame(cam);
@@ -61,9 +30,8 @@ int main(int argc, char *argv[])
     int* frame_size_int = (int*) frame_size;
     printf("Size: %zu\n", frame_size);
     printf("Width: %zu\n", frame_width);
-    printf("Heihgt: %zu\n", frame_height);
-
-    printf("Heihgt: %p\n", frame_size_int);
+    printf("Height: %zu\n", frame_height);
+    printf("Height: %p\n", frame_size_int);
     write(comm_fd, &frame_size_int, 10);
     write(comm_fd, camera_byte, frame_size);
   }
