9c32a2790ac1e0000737bdc1cd088cc212ab7bc8
unknown <anna_sjovall96@hotmail.com>
2017-11-30 11:47:16 +0100

    test1

--- C-camera-master-3e74825e0765a4e308badf65452b6113c68ddc10/examples/our_server.c
+++ C-camera-master-3e74825e0765a4e308badf65452b6113c68ddc10/examples/our_server.c
@@ -22,20 +22,24 @@
 //     return 0;
 // }
 
-struct global_state {
-  camera* cam;
-};
+static pthread_mutex_t global_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void init_global_state(struct global_state* state)
-{
-    state->cam=NULL;
+struct camera_state = {
+  byte* camera_byte;
+  size_t frame_size;
 }
 
 
-void* camera_open_task(void *cam)
+void* camera_open_task(void *state)
 {
-  printf("anna");
-  cam = camera_open();
+  printf("anna\n");
+  camera* cam = camera_open();
+  frame* camera_frame = camera_get_frame(cam);
+  struct camera_state* s = state;
+  pthread_mutex_lock(&global_mutex);
+  s->camera_byte = get_frame_bytes(camera_frame);
+  s->frame_size = get_frame_size(camera_frame);
+  pthread_mutex_unlock(&global_mutex);
   return (void*) (intptr_t) 0;
 }
 
@@ -45,54 +49,33 @@ int main(int argc, char *argv[])
   struct sockaddr_in servaddr;
   struct sockaddr_in servaddr2;
   socklen_t socklen;
-  //Vill skicka över AF_INET, typen SOCK_STREAM sen 0,
-  //Each server needs to “listen” for connections. The above
-  //function creates a socket with AF_INET ( IP Addressing )
-  //and of type SOCK_STREAM. Data from all devices wishing to
-  //connect on this socket will be redirected to listen_fd.
   printf("1\n");
   int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
   bzero( &servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htons(INADDR_ANY);
   servaddr.sin_port = htons(22000);
-  //Prepare to listen for connections from address/port
-  //specified in sockaddr ( Any IP on port 22000 ).
   printf("2\n");
   bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-  // Start Listening for connections , keep at the most 10 connection
-  // requests waiting.If there are more than 10 computers wanting to connect
   printf("3\n");
   // at a time, the 11th one fails to.
   listen(listen_fd, 10);
   printf("4\n");
-  //Accept a connection from any device who is willing to connect, If there
-  //is no one who wants to connect , wait. A file descriptor is returned.
-  //This can finally be used to communicate , whatever is sent by the device
-  // accepted can be read from comm_fd, whatever is written to comm_fd is
-  //sent to the other device.
-  camera* cam = NULL;
   pthread_t camera_open_thread;
-  if(!pthread_create(&camera_open_thread, NULL, camera_open_task, cam)){
+  struct camera_state state;
+  if(!pthread_create(&camera_open_thread, NULL, camera_open_task, state)){
     while (1) {
       printf("4\n");
       int comm_fd = accept(listen_fd, (struct sockaddr*) &servaddr2, &socklen);
-      printf("4\n");
-      frame* camera_frame = camera_get_frame(cam);
-      printf("4\n");
-      byte* camera_byte = get_frame_bytes(camera_frame);
-      size_t frame_size = get_frame_size(camera_frame);
-      size_t frame_height = get_frame_height(camera_frame);
-      size_t frame_width = get_frame_width(camera_frame);
-      int* frame_size_int = (int*) frame_size;
       printf("Size: %zu\n", frame_size);
       printf("Width: %zu\n", frame_width);
       printf("Heihgt: %zu\n", frame_height);
 
-      printf("Heihgt: %p\n", frame_size_int);
       //write(comm_fd, &frame_size_int, 10);
-      write(comm_fd, camera_byte, frame_size);
-      frame_free(camera_frame);
+      pthread_mutex_lock(&global_mutex);
+      write(comm_fd, state->camera_byte, state->frame_size);
+    //  frame_free(camera_frame);
+      pthread_mutex_unlock(&global_mutex);
     }
   }
   }
