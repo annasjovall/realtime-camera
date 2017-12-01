c57b85470e93a118aa78f4acab3cf5bc567284ab
Kevinatorn <kevin.kimaryo@gmail.com>
2017-12-01 21:00:43 +0100

    add imports for compile with javac

--- realtimeproj/src/Client/Main.java
+++ realtimeproj/src/Client/Main.java
@@ -1,5 +1,11 @@
 package Client;
 
+import Client.ClientConnectionThread;
+import Client.ClientReceiver;
+import Client.ClientSharedData;
+import Client.ClientShutdownThread;
+
+
 public class Main {
 
 	private static class StartClient extends Thread {
