a24eda623cd9dfe6c9349f786bcdafca07ecd4fd
Anna Palmqvist Sjövall <anna_sjovall96@hotmail.com>
2017-12-04 21:46:52 +0100

    fix gui

--- client/src/client/UpdateGUIThread.java
+++ client/src/client/UpdateGUIThread.java
@@ -20,25 +20,47 @@ public class UpdateGUIThread extends Thread {
 	public void run() {
 		while (true) {
 			try {
-				if (monitor1.isConnected() && !monitor2.isConnected()) {
-					window.refreshCamera1(monitor1.popUnpackedImage());
+				//only one connected
+				if (!monitor1.isConnected() && !monitor2.isConnected()) {
+					System.out.println("HEj0");
+				} else if (monitor1.isConnected() && !monitor2.isConnected()) {
+					System.out.println("HEj1");
+					if(monitor1.hasImage())
+						window.refreshCamera1(monitor1.popUnpackedImage());
 				} else if (!monitor1.isConnected() && monitor2.isConnected()) {
-					window.refreshCamera2(monitor2.popUnpackedImage());
+					System.out.println("HEj2");
+					if(monitor2.hasImage())
+						window.refreshCamera2(monitor2.popUnpackedImage());
+				//async mode
 				} else if (camerasMonitor.getDisplayMode() == CamerasSharedData.ASYNC_MODE) {
 					window.syncModeRefresh(CamerasSharedData.ASYNC_MODE);
+					DataFrame dataFromMonitor1 = null;
+					DataFrame dataFromMonitor2 = null;
 					if (monitor1.hasImage()) {
-						window.refreshCamera1(monitor1.popUnpackedImage());
+						dataFromMonitor1 = monitor1.popUnpackedImage();
+						window.refreshCamera1(dataFromMonitor1);
 					}
 					if (monitor2.hasImage()) {
-						window.refreshCamera2(monitor2.popUnpackedImage());
+						dataFromMonitor2 = monitor2.popUnpackedImage();
+						window.refreshCamera2(dataFromMonitor2);
 					}
+					if (dataFromMonitor1 != null && dataFromMonitor2 != null) {
+						if (Math.abs(dataFromMonitor1.getTimeStamp() - dataFromMonitor2.getTimeStamp()) < 20) {
+							outOfSync = 0;
+							camerasMonitor.trySetDisplayMode(CamerasSharedData.SYNC_MODE);
+						}
+					}
+				//sync mode
 				} else {
 					window.syncModeRefresh(CamerasSharedData.SYNC_MODE);
 					DataFrame dataFrame1 = monitor1.popUnpackedImage();
 					DataFrame dataFrame2 = monitor2.popUnpackedImage();
 					if (Math.abs(dataFrame1.getTimeStamp() - dataFrame2.getTimeStamp()) > 20) {
 						if (outOfSync > 5) {
-							camerasMonitor.setDisplayMode(CamerasSharedData.ASYNC_MODE);
+							if (!camerasMonitor.trySetDisplayMode(CamerasSharedData.ASYNC_MODE)) {
+								window.refreshCamera1(dataFrame1);
+								window.refreshCamera2(dataFrame2);
+							}
 						} else {
 							window.refreshCamera1(dataFrame1);
 							window.refreshCamera2(dataFrame2);
